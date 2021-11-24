a77166fb5bc006c909cd2a6ddf9dd4a37b1067aa
phuongbd <phuongbd@fireapps.vn>
2021-11-24 21:27:18 +0700

    add

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,13 @@
 </script>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
 
+*{
+    box-sizing: border-box;
+}
+
+body{
+    font-family: "Roboto", sans-serif;
+}
 </style>
